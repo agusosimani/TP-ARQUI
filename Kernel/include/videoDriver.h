5406ea396c01803682952b94414f08a4d51ea2ad
agusosimani <aosimani@itba.edu.ar>
2018-05-17 16:00:53 -0300

    font .c .h

---
+++ Kernel/include/videoDriver.h
@@ -0,0 +1,11 @@
+#ifndef VIDEODRIVER_H
+#define VIDEODRIVER_H
+
+#define SCREEN_WIDTH 80
+#define SCREEN_HEIGHT 25
+#define MAX_DIGITS 20
+#define ROW_LIMIT 25
+
+// funciones para escribir y leer de video
+
+#endif //VIDEODRIVER_H
