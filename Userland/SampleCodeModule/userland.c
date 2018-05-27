c3ccc0d99802c5c943d5fad446050b34ae788a87
agusosimani <aosimani@itba.edu.ar>
2018-05-27 00:51:20 -0300

    refactor userland

--- Userland/SampleCodeModule/userland.c
+++ Userland/SampleCodeModule/userland.c
@@ -1,4 +1,3 @@
-/* sampleCodeModule.c */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,13 +5,7 @@
 #include <ctype.h>
 
 
-char * v = (char*)0xB8000 + 79 * 2;
-
-static int var1 = 0;
-static int var2 = 0;
-
-
 int main() {
-	//printf("Hola\n");
+
 	return 1;
 }
