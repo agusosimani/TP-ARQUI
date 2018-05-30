400531ab04a926d850d297bfd3af57b232860131
cguzzetti <cguzzetti@itba.edu.ar>
2018-05-30 15:52:14 -0300

    strcmp y starts_with

--- Userland/SampleCodeModule/library.c
+++ Userland/SampleCodeModule/library.c
@@ -23,3 +23,30 @@ void sys_clear() {
 void get_char(char* c) {
     systemCall(5,c,0,0);
 }
+
+//Compara 2 cadenas, devuelve 0 si son iguales
+int strcmp(char * s1, char * s2){
+    while(*s1 && *s2 && *s1 == *s2){
+        s1++;
+        s2++;
+    }
+    return *s1 - *s2;
+}
+
+int strncmp(char * s, char * t, unsigned int n) {
+    int i;
+    for (i = 0; i < n-1 && s[i] != '\0' && t[i] != '\0' && s[i] == t[i]; i++)
+        ;
+    return s[i]-t[i];
+}
+
+//Devuelve TRUE si start es subcadena inicial de str, FALSE (0)
+//si esto no pasa
+int starts_with(char * str, char * start){
+    while(*str != 0 && *start != 0){
+        if(*(str++) != *(start++)){
+            return 0;
+        }
+    }
+    return *start == 0;
+}
